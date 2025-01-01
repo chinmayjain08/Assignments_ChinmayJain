c876642e095636e0ba404cfc4a243d3c7d1ebf93
Chinmay Jain <chinmayluhadiya62@gmail.com>
2025-01-01 14:27:17 +0530

    assignment-MathematicalProblem-submit

---
+++ assignmentModuloExponentiation.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+long long mathModExponentiation(long long b, long long n, long long m){
+    long long result = 1;
+    
+    while(n>0){
+        if(n % 2 == 1){
+            result = (result*b) % m;
+        }
+        
+        n = n/2;
+        b = (b*b) %  m;
+    }
+
+    return result;
+}
+
+int main(){
+
+    long long b, n, m;
+    printf("enter values for b, n and m: ");
+    scanf("%lld %lld %lld", &b, &n, &m);
+
+    long long answer = mathModExponentiation(b, n, m);
+    printf("Result : %lld", answer);
+
+    return 0;
+}
